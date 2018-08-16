e1b6bbd1f8375178412cfd1675c1ff904c4a5874
Hamilton Jimenez <hamiltonjbeach7@gmail.com>
2018-08-16 00:00:35 -0400

    Update VideoGamesActivity.java

--- app/src/main/java/com/example/hamilton/jams/VideoGamesActivity.java
+++ app/src/main/java/com/example/hamilton/jams/VideoGamesActivity.java
@@ -38,17 +38,17 @@ public class VideoGamesActivity extends AppCompatActivity {
         songs.add(new Song("Calling", "The World Ends With You", R.drawable.videogames18, R.raw.videogames18));
         songs.add(new Song("Gaur Plains", "Xenoblade", R.drawable.videogames19, R.raw.videogames19));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // Create an {@link SongAdapter}, whose data source is a list of {@link Song}s. The
         // adapter knows how to create list items for each item in the list.
         SongAdapter adapter = new SongAdapter(this, songs);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml layout file
+        // song_list.xml layout file
         ListView listView = findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
+        // Make the {@link ListView} use the {@link SongAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Song} in the list.
         listView.setAdapter(adapter);
     }
 }
